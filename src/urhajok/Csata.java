c0bf7618e8ef0384252dd131e6dccf79119de757
CASOYC <casoyc@inf.elte.hu>
2022-10-21 15:59:21 +0200

    urhalyók

---
+++ src/urhajok/Csata.java
@@ -0,0 +1,14 @@
+package urhajok;
+
+import urhajok.Urhajo;
+
+public class Csata extends Urhajo{
+    public Csata() {
+        tamadas = 4;
+        vedekezes = 4;
+        sebesseg = 3;
+        eletero = 4;
+        femkoltseg = 4;
+        fogyasztas = 4;
+    }
+}
---
+++ src/urhajok/Vadasz.java
@@ -0,0 +1,21 @@
+package urhajok;
+
+import urhajok.Urhajo;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class Vadasz extends Urhajo
+{
+    public Vadasz() throws IOException
+    {
+        tamadas = 6;
+        vedekezes = 2;
+        sebesseg = 4;
+        eletero = 2;
+        femkoltseg = 2;
+        fogyasztas = 2;
+    }
+}
